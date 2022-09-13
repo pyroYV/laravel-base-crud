934009af5c82e416287e9e2559f4199fabca5440
pyroYV <simonerizzo.pvt@gmail.com>
2022-09-13 16:03:54 +0200

    Includes

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Comics | @yeald('title')</title>
+</head>
+<body>
+    @include('includes.header')
+<main>
+    @yield('main-content')
+</main>
+    @include('includes.footer')
+</body>
+</html>

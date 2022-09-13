5ec998136bf4678006b2eabfb4a8d8294a3db007
pyroYV <simonerizzo.pvt@gmail.com>
2022-09-13 16:23:35 +0200

    Model

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.main')
+
+@section('title','Main Comics')
+
+@section('main-content')
+
+    <div class="container-fluid">
+        <div class="row">
+            main content
+        </div>
+    </div>
+
+@endsection
+
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,6 +4,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Comics | @yeald('title')</title>
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <script src="{{asset('js/app.js')}}" defer></script>
 </head>
 <body>
     @include('includes.header')

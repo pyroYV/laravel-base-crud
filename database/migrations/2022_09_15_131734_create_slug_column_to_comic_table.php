a46d28c9a41b35628db1f903bcee7cfd15cc1fbd
pyroYV <simonerizzo.pvt@gmail.com>
2022-09-15 15:25:27 +0200

    Feat: migration to create slug

---
+++ database/migrations/2022_09_15_131734_create_slug_column_to_comic_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlugColumnToComicTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comics', function (Blueprint $table) {
+
+            $table->string('slug');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comics', function(Blueprint $table){
+
+            $table->dropColumn('slug');
+        });
+    }
+}
